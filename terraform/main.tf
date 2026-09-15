416d5bfcd57d190891cbbf74d746534ab6395a59
nelobaba <nelsonumunna2018@gmail.com>
2026-09-15 17:17:53 -0400

    Vend sandbox-aft

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,15 +3,15 @@ module "sandbox_dev" {
 
   control_tower_parameters = {
     AccountEmail              = "manoutechconsulting+4@gmail.com"
-    AccountName               = "sandbox-dev"
-    ManagedOrganizationalUnit = "Management"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Sandbox"
     SSOUserEmail              = "manoutechconsulting+4@gmail.com"
     SSOUserFirstName          = "Manou"
     SSOUserLastName           = "Tech"
   }
 
   account_tags = {
-    "Environment" = "dev"
+    "Environment" = "sandbox"
   }
 
   change_management_parameters = {
@@ -21,31 +21,31 @@ module "sandbox_dev" {
 
   custom_fields = {}
 
-  account_customizations_name = ""
-}
-
-module "sandbox_aft" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "manoutechconsulting+5@gmail.com"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "manoutechconsulting+5@gmail.com"
-    SSOUserFirstName          = "Manoutech"
-    SSOUserLastName           = "IT Firm"
-  }
-
-  account_tags = {
-    "Environment" = "sandbox"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "nelobaba"
-    change_reason       = "Provision sandbox-aft account via AFT (retry after Sandbox OU registered in Control Tower)"
-  }
-
-  custom_fields = {}
-
   account_customizations_name = "sandbox"
 }
+
+# module "sandbox_aft" {
+#   source = "./modules/aft-account-request"
+
+#   control_tower_parameters = {
+#     AccountEmail              = "manoutechconsulting+5@gmail.com"
+#     AccountName               = "sandbox-aft"
+#     ManagedOrganizationalUnit = "Sandbox"
+#     SSOUserEmail              = "manoutechconsulting+5@gmail.com"
+#     SSOUserFirstName          = "Manoutech"
+#     SSOUserLastName           = "IT Firm"
+#   }
+
+#   account_tags = {
+#     "Environment" = "sandbox"
+#   }
+
+#   change_management_parameters = {
+#     change_requested_by = "nelobaba"
+#     change_reason       = "Provision sandbox-aft account via AFT (retry after Sandbox OU registered in Control Tower)"
+#   }
+
+#   custom_fields = {}
+
+#   account_customizations_name = "sandbox"
+# }
